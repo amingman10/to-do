e6cf321e039d54ce727e38b8ac8a6e493cdc2780
Ingman, Amanda Mary <amingman10@gmail.com>
2025-05-20 13:53:18 -0500

    Bonus Lessons

---
+++ bonus/Quiz.py
@@ -0,0 +1,18 @@
+import FreeSimpleGUI as sg
+
+label = sg.Text("What are dolphins?")
+option1 = sg.Radio("Amphibians", group_id="question1")
+option2 = sg.Radio("Fish", group_id="question1")
+option3 = sg.Radio("Mammals", group_id="question1")
+option4 = sg.Radio("Birds", group_id="question1")
+
+window = sg.Window("Quiz",
+                   layout=[[label],
+                           [option1],
+                            [option2],
+                            [option3],
+                            [option4],
+                           ])
+
+window.read()
+window.close()
--- venv/gui.py
+++ venv/gui.py
@@ -6,6 +6,6 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('To-Do App', layout=[[label, add_button], [input_box]])
+window = sg.Window('To-Do App', layout=[[label, add_button, input_box]])
 window.read()
 window.close()
